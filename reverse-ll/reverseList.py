ef18ba4a11dc2cafd99a37f263409ac24e633148
Jihoon Kim <jihoon_kim@apple.com>
2018-05-30 21:42:05 -0700

    Iteratively reverse LL

---
+++ reverse-ll/reverseList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # while next != NULL, create a node w/ the value and set its next to current head, then move the head
+        reversedLL=None
+        while head is not None:
+            print(head.val)
+            newHead=ListNode(head.val)
+            newHead.next=reversedLL
+            reversedLL=newHead
+            head=head.next
+        return reversedLL
